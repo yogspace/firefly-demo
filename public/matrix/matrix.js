f7ce250b1266805e3551d9ef902aaef751b5a802
max.weber <max.weber@meso.design>
2023-08-10 12:57:30 +0200

    Update matrix.js

--- public/matrix/matrix.js
+++ public/matrix/matrix.js
@@ -140,7 +140,7 @@ function getPixels() {
   for (let posY = 0; posY < 16; posY++) {
     //console.log(posY);
     // let row = [];
-    for (let posX = 0; posX < 16; posX++) {
+    for (let posX = 0; posX < 32; posX++) {
       //let pixel = getAveragePixelColor(posX, posY);
       let pixel = {
         id: pixelMatrix[posY][posX],
