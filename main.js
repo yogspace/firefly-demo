6cb05cf83b24986243295fa94241b05ca8bcdc4f
max.weber <max.weber@meso.design>
2023-08-11 09:14:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ expressServer.listen(3001, () => {
   console.log('headless browser is on route http://localhost:3001/matrix');
   console.log('display interface is on route http://localhost:3001/interface');
   console.log('phone interface is on route http://localhost:3001/');
-  //createHeadlessBrowser();
+  createHeadlessBrowser();
 });
 
 async function createHeadlessBrowser() {
