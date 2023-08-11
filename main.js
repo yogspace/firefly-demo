42c0cf46661f221e2ff360a0055488fa6e8b7a13
max.weber <max.weber@meso.design>
2023-08-11 11:51:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ app.get('/interface', (req, res) => {
 });
 
 expressServer.listen(3001, async () => {
-  console.log('\n');
+  console.log('###################\n');
   console.log(`this device:`);
   console.log('display interface is on route https://localhost:3001/interface');
   console.log('phone interface is on route https://localhost:3001/');
@@ -48,6 +48,7 @@ expressServer.listen(3001, async () => {
     `display interface is on route https://${ipAddress}:3001/interface`
   );
   console.log(`phone interface is on route https://${ipAddress}:3001/`);
+  console.log('###################\n');
   createHeadlessBrowser();
 });
 
