700375804382f83d2e57c31899ec29ee1c01d925
max.weber <max.weber@meso.design>
2023-08-12 13:02:21 +0200

    Update web.js

--- public/interface/web.js
+++ public/interface/web.js
@@ -20,17 +20,17 @@ function draw() {
   clear();
   background(config.bgColor);
 
-  // Draw reference circle
+  // Draw reference circle (invisible)
   noFill();
-  stroke(255);
+  stroke(255, 0); // No opacity
   ellipse(sketchWidth / 2, sketchHeight / 2, sketchWidth);
 
   // Draw circles with opacity
   noStroke();
-  fill(255, 0, 0, circleOpacity * 255);
-  ellipse(sketchWidth / 4, sketchHeight / 2, 100);
-  fill(0, 0, 255, (1 - circleOpacity) * 255);
-  ellipse((3 * sketchWidth) / 4, sketchHeight / 2, 100);
+  fill(255);
+  ellipse(sketchWidth / 4, sketchHeight / 2, sketchWidth);
+  fill(255, 255, 255, (1 - circleOpacity) * 255);
+  ellipse((3 * sketchWidth) / 4, sketchHeight / 2, sketchWidth);
 
   if (isCircle1Touched && isCircle2Touched) {
     // Both circles are touched
@@ -58,10 +58,10 @@ function touchStarted() {
     sketchHeight / 2
   );
 
-  if (circle1Dist < 50) {
+  if (circle1Dist < sketchWidth / 2) {
     isCircle1Touched = true;
   }
-  if (circle2Dist < 50) {
+  if (circle2Dist < sketchWidth / 2) {
     isCircle2Touched = true;
   }
 }
