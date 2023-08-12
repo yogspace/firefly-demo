34c3747e52c7544dabd9fc6a9b133d0e15b21d3d
max.weber <max.weber@meso.design>
2023-08-12 17:11:58 +0200

    Update matrix.js

--- public/matrix/matrix.js
+++ public/matrix/matrix.js
@@ -22,7 +22,7 @@ function setup() {
     bgColorInterrupt: color(18, 8, 7),
     bgColorStillInterrupt: color(1, 1, 1),
     fireflyColor: color(210, 180, 200, 40),
-    fireflyColorHighlight: color(255, 255, 255, 255),
+    fireflyColorHighlight: color(220, 220, 220, 255),
   };
 
   bgColor = config.bgColorIdle;
