b7861b93a788d978714ac77c689a4d3d651f8cb8
max.weber <max.weber@meso.design>
2023-08-12 15:29:25 +0200

    initialize

--- public/interface/web.js
+++ public/interface/web.js
@@ -1,8 +1,8 @@
 let sketchWidth;
 let sketchHeight;
-let circleOpacity = 0.5;
-let isCircle1Touched = false;
-let isCircle2Touched = false;
+let btn1;
+let btn2;
+let btnPressedCount = 0;
 
 function setup() {
   sketchWidth = document.getElementById('sketch').offsetWidth;
@@ -14,69 +14,54 @@ function setup() {
   config = {
     bgColor: color(20, 20, 20),
   };
+
+  btn1 = document.getElementById('btn1');
+  btn2 = document.getElementById('btn2');
+
+  btn1.addEventListener('touchstart', btn1Touched);
+  btn2.addEventListener('touchstart', btn2Touched);
+  btn1.addEventListener('touchend', btnReleased);
+  btn2.addEventListener('touchend', btnReleased);
 }
 
 function draw() {
   clear();
   background(config.bgColor);
+}
 
-  // Draw reference circle (invisible)
-  noFill();
-  stroke(255, 0); // No opacity
-  ellipse(sketchWidth / 2, sketchHeight / 2, sketchWidth);
+function btn1Touched() {
+  console.log('Button 1 touched!');
+  btn1.style.opacity = '1'; // Set opacity to 100% when touched
+  btnPressedCount++;
+  checkTouchCount();
+  socket.emit('init', ['A']);
+}
 
-  // Draw circles with opacity
-  noStroke();
-  fill(255);
-  ellipse(sketchWidth / 4, sketchHeight / 2, sketchWidth);
-  fill(255, 255, 255, (1 - circleOpacity) * 255);
-  ellipse((3 * sketchWidth) / 4, sketchHeight / 2, sketchWidth);
+function btn2Touched() {
+  console.log('Button 2 touched!');
+  btn2.style.opacity = '1'; // Set opacity to 100% when touched
+  btnPressedCount++;
+  checkTouchCount();
+  socket.emit('init', ['B']);
+}
 
-  if (isCircle1Touched && isCircle2Touched) {
-    // Both circles are touched
-    // Execute a function or perform an action
-    executeFunctionBothCirclesTouched();
-  } else if (isCircle1Touched) {
-    // Circle 1 is touched
-    // Execute a function or perform an action
-    executeFunctionCircle1Touched();
+function btnReleased() {
+  console.log('Button released!');
+  btn1.style.opacity = '0.1'; // Set opacity back to 10% when released
+  btn2.style.opacity = '0.1'; // Set opacity back to 10% when released
+  btnPressedCount--;
+  checkTouchCount();
+}
+
+function checkTouchCount() {
+  if (btnPressedCount === 2) {
+    thirdFunction();
   }
 }
 
-function touchStarted() {
-  // Check if touches are on the circles
-  let circle1Dist = dist(
-    touches[0].x,
-    touches[0].y,
-    sketchWidth / 4,
-    sketchHeight / 2
+function thirdFunction() {
+  console.log(
+    'Both buttons are touched simultaneously! Third function executed.'
   );
-  let circle2Dist = dist(
-    touches[0].x,
-    touches[0].y,
-    (3 * sketchWidth) / 4,
-    sketchHeight / 2
-  );
-
-  if (circle1Dist < sketchWidth / 2) {
-    isCircle1Touched = true;
-  }
-  if (circle2Dist < sketchWidth / 2) {
-    isCircle2Touched = true;
-  }
-}
-
-function touchEnded() {
-  isCircle1Touched = false;
-  isCircle2Touched = false;
-}
-
-function executeFunctionBothCirclesTouched() {
-  // Your code for the action when both circles are touched
-  console.log('Both circles are touched!');
-}
-
-function executeFunctionCircle1Touched() {
-  // Your code for the action when only circle 1 is touched
-  console.log('Circle 1 is touched!');
+  socket.emit('init', ['A', 'B']);
 }
