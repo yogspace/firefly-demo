2fc990f08aa941128f874a91e86069d5ba29f6a1
max.weber <max.weber@meso.design>
2023-08-13 12:13:24 +0200

    d

--- public/matrix/matrix.js
+++ public/matrix/matrix.js
@@ -262,10 +262,80 @@ let pseudoFirefly = {
   },
 };
 
+let lastPixelMatrix = null;
+function getPixels() {
+  let pixels = [];
+
+  for (let posY = 0; posY < 16; posY++) {
+    for (let posX = 0; posX < 32; posX++) {
+      let r = get(posX, posY)[0];
+      let g = get(posX, posY)[1];
+      let b = get(posX, posY)[2];
+
+      let id = pixelMatrixTranslation[posY][posX];
+
+      if (lastPixelMatrix && hasColorChanged(posX, posY, r, g, b)) {
+        let pixel = {
+          id: id,
+          color: {
+            r: r,
+            g: g,
+            b: b,
+          },
+        };
+        pixels.push(pixel);
+      }
+    }
+  }
+
+  socket.emit('updatePixels', pixels);
+  lastPixelMatrix = createPixelMatrix(); // Aktualisiere das letzte Pixel-Array
+}
+
+function hasColorChanged(x, y, r, g, b) {
+  if (!lastPixelMatrix) {
+    return true;
+  }
+
+  const lastPixel = lastPixelMatrix[y][x];
+  return (
+    lastPixel.color.r !== r ||
+    lastPixel.color.g !== g ||
+    lastPixel.color.b !== b
+  );
+}
+
+function createPixelMatrix() {
+  let matrix = [];
+  for (let posY = 0; posY < 16; posY++) {
+    let row = [];
+    for (let posX = 0; posX < 32; posX++) {
+      let r = get(posX, posY)[0];
+      let g = get(posX, posY)[1];
+      let b = get(posX, posY)[2];
+
+      let id = pixelMatrixTranslation[posY][posX];
+
+      let pixel = {
+        id: id,
+        color: {
+          r: r,
+          g: g,
+          b: b,
+        },
+      };
+
+      row.push(pixel);
+    }
+    matrix.push(row);
+  }
+  return matrix;
+}
+
 function drawFireflies() {
   for (let i = 0; i < fireflies.length; i++) {
-    pseudoFirefly.update();
-    pseudoFirefly.display();
+    fireflies[i].display();
+    fireflies[i].move();
   }
 }
 
@@ -486,3 +556,4 @@ socket.on('end', (data) => {
 });
 
 //UpdatePixels
+const updatePixels = setInterval(getPixels, 10);
